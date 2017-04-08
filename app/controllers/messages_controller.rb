de446be9ebafbbdd42d2573573c56bac73b68296
arm-knt <arima.kent@gmail.com>
2017-04-08 13:08:52 +0900

    edit messages_controller.rb to test whether or not params is given

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,5 +2,11 @@ class MessagesController < ApplicationController
   def index
   end
   def create
+    message = message_params[:message]
+  end
+
+private
+  def message_params
+    params.permit(:message)
   end
 end
