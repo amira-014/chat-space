135a7e992dc383f3b011f25a227fb55b2aac772d
arm-knt <arima.kent@gmail.com>
2017-04-13 08:44:30 +0900

    edit groups_controller to add check_signed_in method

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,11 @@
 class GroupsController < ApplicationController
+before_action :check_signed_in
   def index
-    
+    @name = current_user.name
+  end
+
+  private
+  def check_signed_in
+    redirect_to new_user_session_path unless user_signed_in?
   end
 end
