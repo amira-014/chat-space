bb7895b75eb98ec7c18d8d4bdeeb0a20a071e8b0
arm-knt <arima.kent@gmail.com>
2017-04-26 19:32:08 +0900

    Edit routes.rb and groups_controller.rb to implement user management function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,11 +3,44 @@ before_filter :authenticate_user!
 
   def index
     @user = current_user
+    @groups = Group.all
   end
 
-  def new
+  def show
+    @user = current_user
+    @users = User.all
+    @group = Group.find(params[:id])
+    @groups = Group.all
   end
 
   def edit
+    @group = Group.find(params[:id])
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+  if @group.save
+    redirect_to group_path(@group[:id]), notice: 'グループが作成されました'
+  else
+    redirect_to new_group_path, alert: 'グループの作成に失敗しました'
+  end
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to group_path(@group[:id]), notice: 'グループが編集されました'
+    else
+      redirect_to edit_group_path, alert: 'グループの編集に失敗しました'
+    end
+  end
+
+private
+  def group_params
+    params.require(:group).permit(:name, {user_ids:[]})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
 
-  resources :groups, only: [:get] do
+  resources :groups do
     resources :messages
   end
 end
