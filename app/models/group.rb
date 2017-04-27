a2c8f4d022d653ebeb85952a57fb1ef0837045fb
arm-knt <arima.kent@gmail.com>
2017-04-27 18:09:28 +0900

    Edit group.rb and user.rb to add varidation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
+  validates :name, presence: true
   has_many :groups_users
   has_many :users, through: :groups_users
-  accepts_nested_attributes_for :groups_users
 end
