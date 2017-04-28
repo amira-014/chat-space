891ce57bd920c0ef2d292e89c2c8737cc0e91e2a
arm-knt <arima.kent@gmail.com>
2017-04-28 14:57:40 +0900

    Create message-model and table

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :name, presence: true
+  has_many :messages
   has_many :groups_users
   has_many :groups, through: :groups_users
 end
