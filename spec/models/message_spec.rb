279bf5a454a4c9d37bd7613960011baa0047737f
arm-knt <arima.kent@gmail.com>
2017-05-03 13:53:19 +0900

    Create some files to test post message function

---
+++ spec/models/message_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+describe Message do
+  describe '#create' do
+#bodyが正常に保存されるかどうかのテスト
+    it "is valid with a body" do
+      message = build(:message)
+      expect(message).to be_valid
+    end
+#bodyが空だった場合には保存されないことのテスト
+    it "is invalid without a body" do
+      message = build(:message, body: "")
+      message.valid?
+      expect(message.errors[:body]).to include("を入力してください")
+    end
+
+  end
+end
