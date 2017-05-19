4d1cbf54723fab0f35385198feac6aa81a7435f3
arm-knt <arima.kent@gmail.com>
2017-05-19 23:19:00 +0900

    Set API

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+  def search
+    @users = User.where('name LIKE(?)', "%#{params[:user]}%").where.not(id: current_user.id)
+    respond_to do |format|
+      format.json
+    end
+  end
+
 end
